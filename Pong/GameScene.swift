69909c5e01330a7fea0e02a851ce5af158617f50
Beondel <bengmacmillan@gmail.com>
2017-02-20 21:45:31 -0800

    mvp finished - scores

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -14,11 +14,15 @@ class GameScene: SKScene {
     var player = SKSpriteNode()
     var enemy = SKSpriteNode()
     var ball = SKSpriteNode()
-    
+    var playerScoreLabel = SKLabelNode()
+    var enemyScoreLabel = SKLabelNode()
     var score = [Int]()
     
     override func didMove(to view: SKView) {
         startGame()
+        
+        playerScoreLabel = self.childNode(withName: "playerScoreLabel") as! SKLabelNode
+        enemyScoreLabel = self.childNode(withName: "enemyScoreLabel") as! SKLabelNode
         
         player = self.childNode(withName: "player") as! SKSpriteNode
         enemy = self.childNode(withName: "enemy") as! SKSpriteNode
@@ -34,6 +38,8 @@ class GameScene: SKScene {
     
     func startGame() {
         score = [0, 0]
+        playerScoreLabel.text = "\(score[0])"
+        enemyScoreLabel.text = "\(score[1])"
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -50,25 +56,26 @@ class GameScene: SKScene {
         }
     }
     
-    func addScore(player: SKSpriteNode) {
-        ball.position = CGPoint(x: 0, y: 0)
-        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
-        if (player == self.player) {
+    func addScore() {
+        if (ball.position.y >= enemy.position.y + 30) {
             self.score[0] += 1
+            ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
             ball.physicsBody?.applyImpulse(CGVector(dx: -30, dy: -30))
-        } else if (player == self.enemy) {
+        } else if (ball.position.y <= player.position.y - 30) {
             self.score[1] += 1
+            ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
             ball.physicsBody?.applyImpulse(CGVector(dx: 30, dy: 30))
         }
+        ball.position = CGPoint(x: 0, y: 0)
+        playerScoreLabel.text = "\(score[0])"
+        enemyScoreLabel.text = "\(score[1])"
     }
     
     override func update(_ currentTime: TimeInterval) {
-        enemy.run(SKAction.moveTo(x: ball.position.x, duration: 1.0))
+        enemy.run(SKAction.moveTo(x: ball.position.x, duration: 0.5))
         
-        if (ball.position.y <= player.position.y - 30) {
-            addScore(player: self.enemy)
-        } else if (ball.position.y >= enemy.position.y + 30) {
-            addScore(player: self.player)
+        if (ball.position.y <= player.position.y - 30 || ball.position.y >= enemy.position.y + 30) {
+            addScore()
         }
     }
 }
